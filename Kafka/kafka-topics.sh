31d255f4be4d2f5fe18660eab3d2191d2e3cdf04
codsalah <salahhamada63@gmail.com>
2025-12-12 13:15:02 +0200

    Refactor Kafka scripts to use environment variables

--- Kafka/kafka-topics.sh
+++ Kafka/kafka-topics.sh
@@ -1,1 +1,7 @@
-kafka-topics --bootstrap-server localhost:9092 --create --topic earthquake_raw --partitions 3 --replication-factor 1
+#!/bin/bash
+
+# Load environment variables with defaults
+KAFKA_PORT=${KAFKA_INTERNAL_PORT:-9092}
+TOPIC=${KAFKA_TOPIC:-earthquake_raw}
+
+kafka-topics --bootstrap-server localhost:${KAFKA_PORT} --create --topic ${TOPIC} --partitions 3 --replication-factor 1
