2a3a84cbba6551b91e4e09d9fb64b40c3f32b714
Salma Nour-Eldeen <134361394+salma-nour-eldeen6@users.noreply.github.com>
2026-01-10 21:19:42 +0200

    Remove: Kafka topic configuration for airports

--- Kafka/kafka-topics-airport.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Load environment variables with defaults
-KAFKA_PORT=${KAFKA_INTERNAL_PORT:-9092}
-TOPIC=${KAFKA_TOPIC_AIRPORT:-airport_stream}
-
-echo "Creating Kafka topic: ${TOPIC}"
-kafka-topics --bootstrap-server localhost:${KAFKA_PORT} --create --topic ${TOPIC} --partitions 3 --replication-factor 1 --if-not-exists
